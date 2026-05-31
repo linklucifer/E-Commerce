74583b624a2684d440426e86ab00d190fa55eb79
Aditya Kumar Singh <linklucifer1@gmail.com>
2026-05-31 20:17:39 +0530

    Setup backend and added userModel and userSchema also setup the database

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,6 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const connectDB = async () => {
+
+    mongoose.connection.on('connected', () => {
+        console.log("DB Connected")
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 
 }
 
