67e1de605ff742295c4f00e4979ca83446610e4c
Aditya Kumar Singh <linklucifer1@gmail.com>
2026-06-02 18:49:35 +0530

    Added more functions and product routes in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,34 @@ import userModel from "../models/userModel.js";
 
 
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET)
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // Route for user login
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exists" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: "Invalid credetials" })
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Route for user registration
@@ -47,11 +69,11 @@ const registerUser = async (req, res) => {
 
         const token = createToken(user._id)
 
-        res.json({success: true, token})
+        res.json({ success: true, token })
 
     } catch (error) {
         console.log(error)
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
