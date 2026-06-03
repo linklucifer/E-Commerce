ff5a1c8a39dfc04af844a778ee898993d939ead4
Aditya Kumar Singh <linklucifer1@gmail.com>
2026-06-03 14:30:54 +0530

    Added more features in product route and product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,29 @@
-
+import { v2 as cloudinary } from 'cloudinary'
 
 // function for add product
 const addProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body
 
+        const image1 = req.files.image1 && req.files.image1[0]
+        const image2 = req.files.image2 && req.files.image2[0]
+        const image3 = req.files.image3 && req.files.image3[0]
+        const image4 = req.files.image4 && req.files.image4[0]
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: "image" });
+                return result.secure_url
+            })
+        )
+        
+        res.json({})
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // function for list product
