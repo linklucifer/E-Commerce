e03c1a8db4d91deba79359a9064d0491c26b5c06
Aditya Kumar Singh <linklucifer1@gmail.com>
2026-07-03 11:20:50 +0530

    Added more login features and connected the frontend with the backend

---
+++ backend/routes/cartRoute.js
@@ -0,0 +1,4 @@
+import express from 'express'
+import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js'
+
+
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const Login = () => {
 
-  const [currentState, setCurrentState] = useState('Sign Up');
+  const [currentState, setCurrentState] = useState('Login');
   const { token, setToken, navigate, backendUrl } = useContext(ShopContext)
 
   const [name, setName] = useState('')
@@ -41,6 +41,12 @@ const Login = () => {
       toast.error(error.message)
     }
   }
+
+  useEffect(() => {
+    if(token){
+      navigate('/')
+    }
+  }, [token])
 
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
