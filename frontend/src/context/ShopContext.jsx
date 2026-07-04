c1d8fa1dc6d877dd146fcd2b33cc4dc6367220e9
Aditya Kumar Singh <linklucifer1@gmail.com>
2026-07-04 11:03:36 +0530

    Fix the login feature bug and connect the frontend with the database by which the cart data should intact

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -42,6 +42,17 @@ const ShopContextProvider = (props) => {
         }
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } })
+
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -65,6 +76,18 @@ const ShopContextProvider = (props) => {
         let cartData = structuredClone(cartItems);
         cartData[itemId][size] = quantity;
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, {headers: {token}})
+
+            } catch (error) {
+                console.log(error)
+                toast.error(error.message)
+            }
+
+        }
     }
 
     const getCartAmount = () => {
@@ -101,6 +124,20 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}})
+            if(response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         getProductsData()
     }, [])
@@ -108,6 +145,7 @@ const ShopContextProvider = (props) => {
     useEffect(() => {
         if (!token && localStorage.getItem('token')) {
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
     }, [])
 
